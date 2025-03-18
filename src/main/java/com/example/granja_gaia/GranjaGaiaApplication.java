74dab033a1472f3028aa5495a78e1f0049c02cdd
cintia <ccastelgonzalez@safareyes.es>
2025-03-18 20:39:45 +0100

    🎉 Inicio del proyecto

---
+++ src/main/java/com/example/granja_gaia/GranjaGaiaApplication.java
@@ -0,0 +1,13 @@
+package com.example.granja_gaia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GranjaGaiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GranjaGaiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/granja_gaia/GranjaGaiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.granja_gaia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GranjaGaiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
