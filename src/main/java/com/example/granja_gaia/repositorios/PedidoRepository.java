2ceda10221833cd441405bed78977ff6d2dd0a8d
cintia <ccastelgonzalez@safareyes.es>
2025-05-24 13:53:34 +0200

    ♻️Actualización de pedidoRepository

--- src/main/java/com/example/granja_gaia/repositorios/PedidoRepository.java
+++ src/main/java/com/example/granja_gaia/repositorios/PedidoRepository.java
@@ -9,10 +9,15 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 public interface PedidoRepository extends JpaRepository<Pedido, Integer> {
-    List<Pedido> findByClienteId(Integer clienteId);
+    @Query("SELECT p FROM Pedido p join fetch p.cliente  WHERE p.estado != 'pendiente' and p.cliente.id = :clienteId")
+    List<Pedido> findByClienteOrders(Integer clienteId);
+
 
     @Query("SELECT p FROM Pedido p WHERE p.fecha BETWEEN :fechaInicio AND :fechaFin")
     List<Pedido> findByFechaBetween(
             @Param("fechaInicio") LocalDateTime fechaInicio,
             @Param("fechaFin") LocalDateTime fechaFin);
+
+    @Query("SELECT p FROM Pedido p join fetch p.cliente  WHERE p.estado != 'pendiente'")
+    List<Pedido> findAllOrders();
 }
